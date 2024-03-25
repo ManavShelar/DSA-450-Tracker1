ed869036c960492b31432ed7404013bf3deb574c
Manav Shelar <manavshelar2003@gmail.com>
2024-03-25 22:16:21 +0530

    Initial project setup

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import { Flex, Link, Text } from '@chakra-ui/react'
+
+const Footer = () => {
+    const originalDsaSheetLink =
+        'https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/'
+    const originalAuthorLinkedInLink = 'https://in.linkedin.com/in/rajarvp'
+    const gitHubLink = 'https://github.com/aditya-190/a2zdsa'
+
+    return (
+        <Flex
+            
+        >
+            <Text
+                fontWeight={'md'}
+                fontSize={'xs'}
+                fontFamily={'customFamily'}
+                fontStyle={'normal'}
+                color={'secondaryColor'}
+                textAlign={'center'}
+            >
+               
+                
+            </Text>
+
+           
+        </Flex>
+    )
+}
+
+export default Footer
